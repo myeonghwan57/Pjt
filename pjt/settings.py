78faf8cc712b8a6699dc404dd351b59bcca2bbe1
myeonghwna57 <myeonghwan57@gmail.com>
2022-11-18 17:09:28 +0900

    배포

--- pjt/settings.py
+++ pjt/settings.py
@@ -221,12 +221,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "static",
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
